a4aff2ca6eb252afcb716b956f88479886283913
Goh'Dan <gohdan@gohdan.ru>
2015-08-25 23:47:19 +0300

    pages: added admin actions list

--- themes/default/config.php
+++ themes/default/config.php
@@ -193,6 +193,21 @@ $config['pages']['page_title'] = "Kshema";
 $config['pages']['sort_list_by'] = "name";
 $config['pages']['default_action'] = "view";
 $config['pages']['css'] = ""; // additional CSS file
+$config['pages']['admin_actions'] = array(
+	'admin',
+	'create_tables',
+	'drop_tables',
+	'update_tables',
+	'categories_view',
+	'categories_add',
+	'categories_edit',
+	'categories_del',
+	'list_view',
+	'add',
+	'edit',
+	'del',
+	'transfer'
+);
 
 $config['news']['last_news_qty'] = 3;
 $config['news']['news_template'] = "view";
