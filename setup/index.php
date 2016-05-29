f7dfab81ec6a8d64e663d2df5491d8559db6cc38
Goh'Dan <gohdan@gohdan.ru>
2016-05-30 02:09:29 +0300

    Changed settings array

--- setup/index.php
+++ setup/index.php
@@ -116,20 +116,21 @@ if (isset($_POST['do_save']))
 
 \$config['modules']['default_module'] = \"frontpage\";
 
-\$config['modules']['core'] = array();
-\$config['modules']['core'][] = \"auth\";
-\$config['modules']['core'][] = \"base\";
-\$config['modules']['core'][] = \"db\";
-\$config['modules']['core'][] = \"files\";
-\$config['modules']['core'][] = \"frontpage\";
-\$config['modules']['core'][] = \"hooks\";
-\$config['modules']['core'][] = \"menu\";
-\$config['modules']['core'][] = \"modules\";
-\$config['modules']['core'][] = \"templater\";
-\$config['modules']['core'][] = \"themes\";
-\$config['modules']['core'][] = \"updater\";
-\$config['modules']['core'][] = \"uploads\";
-\$config['modules']['core'][] = \"users\";
+\$config['modules']['core'] = array(
+	\"auth\",
+	\"base\",
+	\"db\",
+	\"files\",
+	\"frontpage\",
+	\"hooks\",
+	\"menu\",
+	\"modules\",
+	\"templater\",
+	\"themes\",
+	\"updater\",
+	\"uploads\",
+	\"users\"
+);
 
 \$config['modules']['installed'] = array();
 
